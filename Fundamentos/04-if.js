338ec6ae2cf7e07a9637950f58655b59cfe0dd1e
Liliana Cerda <lilianacerda33@gmail.com>
2021-05-03 20:58:51 -0400

    Actualizafición 3 de mayo

---
+++ Fundamentos/04-if.js
@@ -0,0 +1,60 @@
+//Estructura de control
+//If.....else
+
+//  se cumple la condición entonces realizar la acción si no s cumple la condicion ebtobceshacer otra cosa
+// leSi
+let num =9;
+
+if (num ===10) {
+    console.log('El numero es igual a 10');
+
+}else if (num > 10) {
+        console.log('El numero es mayor que 10');
+    }elnsolese {
+        console.log('el numero es meno que 10');
+    }
+
+// === compara valores
+// = asigna
+
+if(isNaN(num)){
+console.log('(Ingrese un número');
+}
+
+//fiesta
+//Solo puede entrar un mayor de edad osea edad>=18
+//si no es mayor edad debe ir acompañado de un tutor
+
+let edad=21
+let tutor=false
+
+if(edad >=18){
+console.log('Bienvenido a la JOda');
+}else if(tutor===true){
+   console.log'Bienvenido ala joda no se separe de su tutor'; 
+}else{
+    console.log('salda de aqui, uste no puede disfruta de la vida');
+}
+
+if(edad>18 && tutor===false){
+    console.error('Salga de aqui');
+
+}else if(edad>=18 || tutor===true){
+    console.log('Bienvenido a la Joda');
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app.js
@@ -0,0 +1,14 @@
+//bucles
+//while
+/*
+mientras condiciób entonces accion*/
+let vecesSumar=10;
+let totalSuma=0;
+
+while(vecesSumar>0)
+{
+    totalSuma +=1
+    vecesSumar-=1
+    consola.log(`totalSuma: ${totalSuma} |
+    vecesSumar: ${vecesSumar}`)
+}
